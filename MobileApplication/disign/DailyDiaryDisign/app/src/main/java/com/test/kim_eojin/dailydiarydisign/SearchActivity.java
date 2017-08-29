66ff453b512d25c6137be59f59ac387314d2bd77
KimEJ <kimuj5090@gmail.com>
2017-08-29 22:37:17 +0900

    .
    .

---
+++ MobileApplication/disign/DailyDiaryDisign/app/src/main/java/com/test/kim_eojin/dailydiarydisign/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.test.kim_eojin.dailydiarydisign;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_search);
+        //createGUI();
+    }
+}
