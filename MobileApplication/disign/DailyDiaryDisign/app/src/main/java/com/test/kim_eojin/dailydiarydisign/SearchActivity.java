ebf71e02f580d10fa7a9531408da18e7a8410772
syshinkr <syshinkr228@gmail.com>
2017-09-11 21:46:26 +0900

    XML
    SearchActivity
    WriteActivity
    오류 나는데 기본 코드임

--- MobileApplication/disign/DailyDiaryDisign/app/src/main/java/com/test/kim_eojin/dailydiarydisign/SearchActivity.java
+++ MobileApplication/disign/DailyDiaryDisign/app/src/main/java/com/test/kim_eojin/dailydiarydisign/SearchActivity.java
@@ -1,16 +1,13 @@
 package com.test.kim_eojin.dailydiarydisign;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.WindowManager;
+import android.os.Bundle;
 
 public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_search2);
     }
 }
