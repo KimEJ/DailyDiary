71de881b61f2aa433012d25328bd251928f2aae7
Sohyeon Park <thgus4618@naver.com>
2017-09-12 22:22:00 +0900

    First Commit

---
+++ DailyDiary/app/src/main/java/dsm/dailydiary/MainActivity.java
@@ -0,0 +1,18 @@
+package dsm.dailydiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+/** 캘린더 */
+/** 네비바 */
+/** 날씨 */
+/** 타이틀바 커스텀 */
