738c2e11fb817a61a405f5e8b63d236e4c767cc9
syshinkr <syshinkr228@gmail.com>
2017-09-19 20:08:55 +0900

    Modify Toolbar

--- DailyDiary/app/src/main/java/dsm/dailydiary/WriteActivity.java
+++ DailyDiary/app/src/main/java/dsm/dailydiary/WriteActivity.java
@@ -1,13 +1,23 @@
 package dsm.dailydiary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class WriteActivity extends AppCompatActivity {
-
+    TextView textView;
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        textView = (TextView) findViewById(R.id.title);
+        textView.setText("write");
+        imageButton = (ImageButton) findViewById(R.id.search);
+        imageButton.setBackground(null);
+        imageButton.setImageResource(R.drawable.check);
+
     }
 }
